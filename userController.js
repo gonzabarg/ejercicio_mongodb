242b22c8d49ef7a14a143b5d14c2d58a0e5ee6ae
mauroraviolo23 <mauro.raviolo@gmail.com>
2022-02-21 14:45:55 -0300

    x

--- userController.js
+++ userController.js
@@ -3,42 +3,42 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "ha_ejercicio_20",
+	host: "localhost",
+	user: "root",
+	password: "root",
+	database: "ha_ejercicio_20",
 });
 
 connection.connect(function (err) {
-  if (err) throw err;
+	if (err) throw err;
 
-  console.log("Conectado");
+	console.log("Conectado");
 });
 
 module.exports = {
-  show: (req, res) => {
-    connection.query("SELECT * FROM users", function (err, users) {
-      if (err) throw err;
+	show: (req, res) => {
+		connection.query("SELECT * FROM users", function (err, users) {
+			if (err) throw err;
 
-      console.log(users);
+			console.log(users);
 
-      res.render("usuarios", { users: users });
-    });
-  },
+			res.render("usuarios", { users: users });
+		});
+	},
 
-  delete: (req, res) => {
-    console.log("Eliminar usuario");
+	delete: (req, res) => {
+		console.log("Eliminar usuario");
 
-    const userID = req.params.id;
+		const userID = req.params.id;
 
-    connection.query(
-      "DELETE FROM users WHERE id = " + connection.escape(userID),
-      function (err, users) {
-        if (err) throw err;
-        console.log(users);
+		connection.query(
+			"DELETE FROM users WHERE id = " + connection.escape(userID),
+			function (err, users) {
+				if (err) throw err;
+				console.log(users);
 
-        res.redirect("/usuarios");
-      }
-    );
-  },
+				res.redirect("/usuarios");
+			}
+		);
+	},
 };
